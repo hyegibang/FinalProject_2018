6106eaec7b03ad0088d645ab4569de1a8ca5fcc6
jzerez <Jonathan.zerez@students.olin.edu>
2018-04-16 23:59:20 -0400

    server

--- Sockets/server.py
+++ Sockets/server.py
@@ -1,7 +1,12 @@
 import socket
+<<<<<<< HEAD
 HOST = '0.0.0.0'
 PORT = 8855
 print(socket.gethostname())
+=======
+HOST = '192.168.33.217'
+PORT = 13379
+>>>>>>> 35722ac0407069c3cda0b3b3a03a6ef2c34a359e
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostbyname('0.0.0.0'), PORT))
 s.listen(1)
